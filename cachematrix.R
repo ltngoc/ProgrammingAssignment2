d409d020d9389c530a3e1b477d8ad204a9b79658
ltngoc <ltngoc@comp.nus.edu.sg>
2014-09-22 00:19:57 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,6 @@
+## The following makeCacheMatrix function takes an empty matrix as an input 
+## and returns another matrix with 4 properties which are also functions namely: set, get, setInv, getInv 
+
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -15,8 +18,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+## The following cacheSolve function takes output of the makeCacheMatrix function as an input 
+## and ouput of cacheSolve function is the reverse of matrix input.
+## The function checks if the special matrix was inversed 
+## If it was already inversed, the prevouse result is returned. If not, solve() function will return the inverse of matrix
+
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   inv <- x$getInv()
   if(!is.null(inv)) {
     message("getting cached data")
@@ -27,3 +34,5 @@ cacheSolve <- function(x, ...) {
   x$setInv(inv)
   inv
 }
+
+## How to run the code is in the guide.md file
